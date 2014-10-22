005160a380a52dd6d701548ed277ce8faaf20af4
Marinin Tim <marinintim@gmail.com>
2014-10-23 01:37:57 +0400

    typos are adding to nedb

---
+++ index.js
@@ -0,0 +1,101 @@
+var Hapi = require('hapi'),    
+    Good = require('good'),
+    Path = require('path'),
+    Datastore = require('nedb')
+    //Notifier = require('./notifier.js')
+
+
+var server = new Hapi.Server(5051)
+
+server.views({
+    engines: {
+        html: require('handlebars')
+    },
+    path: Path.join(__dirname, 'templates')
+})
+
+var Typos = new Datastore({
+    filename: 'db/typos',
+    autoload: true
+})
+
+var Users = new Datastore({
+    filename: 'db/users',
+    autoload: true
+})
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function(request, reply){
+        console.log(request)
+        reply.view('landing')
+    }
+})
+
+server.route({
+    method: 'GET',
+    path: '/test/{owner}',
+    handler: function(request, reply){
+        console.log(request.params.owner.toString())
+        Typos.find({owner: request.params.owner}, function(err, typos) {
+            if (err) throw Error()
+            reply.view('list', {typos: typos})
+        })
+    }
+})
+
+server.route({
+    method: 'POST',
+    path: '/oh',
+    handler: function(request, reply){
+        var typo = {
+            owner: request.payload.owner,
+            text: request.payload.text,
+            classes: request.payload.classes,
+            tag: request.payload.tag,
+            url: request.payload.url,
+            additional: request.payload.additional || null
+        }
+
+        Typos.insert(typo, function(err, savedTypo) {
+            console.log('fukken saved:', savedTypo)
+            //Notifier.update({owner: savedTypo.owner, typoId: savedTypo._id})
+            reply(savedTypo)
+        })
+    }
+})
+
+server.route({
+    method: 'GET',
+    path: '/js',
+    handler: function(request, reply) {
+        reply.file('typo.js')
+    }
+})
+
+server.route({
+    method: 'GET',
+    path: '/css',
+    handler: function(request, reply) {
+        reply.file('typo.css')
+    }
+})
+
+server.route({
+    method: 'GET',
+    path: '/public/{filename}',
+    handler: function(request, reply) {
+        reply.file(Path.join(__dirname, 'public', request.params.filename))
+    }
+})
+
+server.pack.register(Good, function (err) {
+    if (err) {
+        throw err
+    }
+
+    server.start(function () {
+        server.log('info', 'Server of Mistype is running at: ' + server.info.uri)
+    })
+})
--- typo.js
+++ typo.js
@@ -1,50 +1,107 @@
 // Written by Marinin Tim, licensed under MIT license.
 
 
-;(function(window){
-  // return
+;(function(window, encodulator, noty, Mistype, config){
+
+  // Simply make request
+  var requster = function(opts) {
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function(){
+      if (req.readyState === 4 && typeof opts.callback === 'function') {
+        opts.callback(req.responseText);
+      }
+    };
+    req.open(opts.method || 'get', opts.url, true);
+    req.send(opts.data);
+  }
+
+  // encode json obj to formdata
   var encodulator = function(opts) {
-    var memo = '?id=' + (encodeURIComponent(config.id) || 'none') + '&'
+    var memo = new FormData()
+    memo.append('owner', config.owner || 'none')
     for (var key in opts) {
-      memo += encodeURIComponent(key) + '=' + encodeURIComponent(opts[key]) + '&'
+      if (opts[key] != null) memo.append(key, opts[key])
     }
     return memo;
   }
 
-  var Mistype = function(opts) {
-    this.opts = opts;
-    window.addEventListener('keyup', function(e) {
-      if (e.which == 13 && e.ctrlKey) {
-        this.send(this.get())
-      }
-    }.bind(this))
-  }
+  // Simple notification
+  var noty = function(e){
+      var noty = document.createElement('div');
+      noty.classList.add('mistype-notification')
+      noty.innerHTML = config.text || 'Mistype was sended to our editors, thanks for your attentiveness';
+      document.body.appendChild(noty)
 
+      setTimeout(function(){
+        noty.classList.add('in-action');
+        setTimeout(function(){
+          noty.classList.remove('in-action')
+          setTimeout(noty.parentNode.removeChild.bind(noty.parentNode, noty), config.timeoutRemove || 3000);
+        }, config.timeoutHide || 3000)
+      }, config.timeoutShow || 100)
+    }
+
+  var Mistype = function() {
+    for (var key in config.listeners) {
+      window.addEventListener(key, config.listeners[key].bind(this))
+    }
+  }
 
   Mistype.prototype.get = function() {
     var selection = window.getSelection()
-    if (selection.anchorNode !== null) {
+
+    if (selection.anchorNode !== null && selection.toString() !== '') {
       return {
         text: selection.toString(),
         classes: Array.slice.call(selection.anchorNode.parentElement.classList, 0),
         tag: selection.anchorNode.parentNode.tagName,
-        url: window.location.href 
+        url: window.location.href,
+        additional: typeof config.additional === 'function' ? config.additional() : config.additional
+      }
+    } else {
+      return null;
+    }
+  }  
+
+  Mistype.prototype.send = function(data, callback) {
+    requster({
+      method: 'POST',
+      url: config.endpoint,
+      data: encodulator(data),
+      callback: callback
+    })
+  }
+
+  var config = JSON.parse(document.querySelector('script[data-typo]').innerHTML)
+
+  if(!config.endpoint) {
+    config.endpoint = '/oh'
+  }
+
+  if(!config.listeners) {
+    config.listeners = {
+      keyup: function(e) {
+        if(e.target === document.body && e.which == 13 && e.ctrlKey) {
+          var text = this.get()
+          if (text !== null) {
+            this.send(text, noty)
+          }
+        }
       }
     }
   }
 
-  Mistype.prototype.send = function(data, callback) {
-    var req = new XMLHttpRequest();
-    req.onreadystatechange = function(){
-      if (req.readyState === 4 && typeof callback === 'function') {
-        callback(req.responseText);
+  if (!config.nocss) {
+    requster({
+      url: '/css', 
+      callback: function(response) {
+        var styleEl = document.createElement('style')
+        styleEl.innerHTML = response
+        document.body.appendChild(styleEl)
       }
-    };
-    req.open('get', '//oh.mistype.co/' + encodulator(data), true);
-    req.send();
+    })
   }
 
-  var config = JSON.parse(document.querySelector('script[src$="typo.js"]').innerHTML)
-
-  return window.mistype = new Mistype(config);
+  return window.mistype = new Mistype();
+  
 })(window)
