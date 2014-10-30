238c34630a770c7a9e82fdceb755c4701812ada0
Tim Marinin <marinin@playform.com>
2014-10-30 16:08:12 +0300

    wut fixes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var Hapi = require('hapi'),
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-var server = new Hapi.Server(server_port)
+var server = new Hapi.Server(server_ip_address, server_port)
 
 server.views({
     engines: {
